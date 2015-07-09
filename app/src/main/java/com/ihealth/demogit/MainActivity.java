d8cd414d7a96064609fc77984e4a3ea8f20df6b7
AllenGao <84370581@163.com>
2015-07-09 15:23:16 +0800

    change

--- app/src/main/java/com/ihealth/demogit/MainActivity.java
+++ app/src/main/java/com/ihealth/demogit/MainActivity.java
@@ -1,8 +1,7 @@
 package com.ihealth.demogit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("aa", "DemoGit_1");
     }
 
     @Override
